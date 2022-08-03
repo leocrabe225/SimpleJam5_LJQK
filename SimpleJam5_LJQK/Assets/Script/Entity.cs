bdc7e423d7cffa8a0fcac5defcc84cc4782859a0
jessy-damoiseau <jessydamoiseau@gmail.com>
2022-08-03 12:46:56 +0200

    create Entity script

---
+++ SimpleJam5_LJQK/Assets/Script/Entity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public Entity (int )
+    
+    void Update()
+    {
+        
+    }
+
+    private int life;
+    
+}
