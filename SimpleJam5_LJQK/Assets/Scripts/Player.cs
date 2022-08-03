a523f38826cc0589d6f27f3593cc0bb87bcf9eb6
leocrabe225 <petitscrounchs@yahoo.fr>
2022-08-03 14:29:10 +0200

    Added Player Controller

--- SimpleJam5_LJQK/Assets/Scripts/Player.cs
+++ SimpleJam5_LJQK/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : Entity
 {
     // Start is called before the first frame update
+    private float speed = 5;
     void Start()
     {
         
@@ -13,6 +14,37 @@ public class Player : Entity
     // Update is called once per frame
     void Update()
     {
+        Vector2 to_move = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) {
+            to_move += Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            to_move += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            to_move += Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            to_move += Vector2.right;
+        }
+        to_move = to_move.normalized * speed * Time.deltaTime;
+        transform.Translate(to_move);
         Debug.Log("boite en fer (c'est mieux)");
+        if (transform.position.x > 10.7f) {
+            Vector2 temp = new Vector2(-10.7f, transform.position.y);
+            transform.position = temp;
+        }
+        if (transform.position.x < -10.7f) {
+            Vector2 temp = new Vector2(10.7f, transform.position.y);
+            transform.position = temp;
+        }
+        if (transform.position.y > 5f) {
+            Vector2 temp = new Vector2(transform.position.x, -5f);
+            transform.position = temp;
+        }
+        if (transform.position.y < -5f) {
+            Vector2 temp = new Vector2(transform.position.x, 5f);
+            transform.position = temp;
+        }
     }
 }
